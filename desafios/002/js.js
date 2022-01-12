d71e6451b0514759428a8c8d8bff9d3426b895e3
VICENTE-DOSSANTOS <gv55172@gmail.com>
2022-01-12 16:42:01 -0300

    atualizado

--- desafios/002/js.js
+++ desafios/002/js.js
@@ -3,61 +3,63 @@ function verificar() {
     var ano = data.getFullYear()
     var fano = document.querySelector ('input#txtano') 
     var res = document.querySelector ('div#res')
-    var img = document.createElement('img')
-    img.setAttribute('id', 'foto')
-    if (fano.value.lenght ==0 || fano.value > ano) {
+   
+    if (fano.value.lenght == 0 || fano.value > ano) {
         window.alert ('[ERRO] Verifique os dados e tente novamente')
      } else {
          var fsex = document.getElementsByName('radsex')
+        
          var idade = ano - Number(fano.value)
-         var genero = '' }
-         if (fsex[0].checked) {
-             genero = 'Homem'
-            if (idade >= 0 && idade < 10) {
-                //criança
-                img.setAttribute('src', 'imagens/foto-bebe-m.png')
-            } else if ( idade < 21) {
-                // jovem
-                img.setAttribute ('src', 'imagens/foto-jovem-m.png')
-            } else if (idade < 50) {
-                //adulto
-                img.setAttribute ('src','imagens/foto-adulto-m.png')
+         var genero = '' 
+         var img = document.createElement('img')
+         img.setAttribute('id', 'foto')
+         
+        
+        
+        
+        }
 
-            } else {
-                //idoso
-                img.setAttribute('src','imagens/foto-idoso-m.png')
-            }
+         if(fsex[0].checked) {
+             genero = 'homem'
             
-            
-            } else if (fsex[1].checked) {
-                genero = 'Mulher'
-            } if (idade >=0 && idade < 10) {
-                //criança
-                img.setAttribute('src', 'imagens/foto-bebe-f.png')
-            } else if (idade < 21) {
-                //jovem
-                img.setAttribute ('src', 'imagens/foto-jovem-f.png')
-            }else if (idade < 50) {
-                //adulto
-                img.setAttribute ('src','imagens/foto-adulto-f.png')
             } else {
-                //idoso
-                img.setAttribute('src','imagens/foto-idoso-f.png')
-            }
+                genero = 'mulher'
+            } 
+             if (idade <= 10){
+                 img.setAttribute ('src', `${genero}/foto-bebe-m.png`)
+             } else if (idade < 21) {
+                img.setAttribute ('src', `${genero}/foto-jovem-m.png`)
+
+             } else if ( idade < 50 ) {
+                img.setAttribute ('src', `${genero}/foto-adulto-m.png`)
+             } else  if (idade >=60){
+                img.setAttribute ('src', `${genero}/foto-idoso-m.png`)
+             }
+             if (idade <= 10 && genero == 'mulher') {
+                 img.setAttribute ('src', `${genero}/foto-bebe-f.png` )
+             } else if ( idade < 21 && genero == 'mulher') {
+                img.setAttribute ('src', `${genero}/foto-jovem-f.png` )
+ 
+             } else if ( idade < 50 && genero == 'mulher'){
+                img.setAttribute ('src', `${genero}/foto-adulto-f.png` )
+             } else if( idade >= 60 && genero =='mulher' ) {
+                img.setAttribute ('src', `${genero}/foto-idoso-f.png` )
+             }
 
 
-           
-         
-         
-         
-         
-         
-         
-         
-         
-         
+
            res.innerHTML = `Detectamos ${genero} com ${idade} anos `
            res.appendChild(img)
+         
+         
+         
+         
+         
+         
+         
+         
+         
+           
          
             } 
                  
