d44e49ccc5382b3c352cc5d27e8a704ccb6b9723
VICENTE-DOSSANTOS <gv55172@gmail.com>
2022-01-13 21:17:35 -0300

    trabalhando com o select

---
+++ ex008/script.js
@@ -0,0 +1,18 @@
+function gerar () {
+    var num = document.getElementById ('tab')
+    var tab = document.getElementById ('seltab')
+    if (num.value.length == 0 ) {
+        window.alert('[ERRO] Insira um número!')
+    } else {
+         var n = Number(num.value)
+         var c = 1 
+         tab.innerHTML = ''
+         while (c <= 10) {
+             var item = document.createElement('option')
+             item.text = `${n} x ${c} = ${n*c} `
+             tab.appendChild (item)
+             c++
+         }
+
+    }
+}
