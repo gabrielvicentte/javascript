d0e5779490c5b39baff09f2d71ea79edba69436b
gabrielvicentte <gv55172@gmail.com>
2022-02-14 01:33:41 -0300

    atualizado

---
+++ desafios/009/index.js
@@ -0,0 +1,21 @@
+let c = 0
+function adicionar() {
+    c++
+    let txt = document.querySelector('#txto')
+    let anot =  document.querySelector('#anot')
+    texto = (txt.value)
+    anot.innerHTML +=  `<ul class="del" id="${c}"> <li> <p >${texto}  <input id="botao"  type="button" value="X" onclick="deletar(${c})"></p> </li> </ul> `
+    txt.value = ''
+    txt.focus()
+   
+}
+function deletar(id){
+    let tarefa = document.getElementById(id)
+    tarefa.remove()
+ 
+
+}
+function apagar(){
+   anot.innerHTML = ''
+}
+
