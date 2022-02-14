a113bcc0ab9f75d41c0bbc21afd8e6dbe8d79882
gabrielvicentte <gv55172@gmail.com>
2022-02-14 11:00:51 -0300

    atualizado

--- desafios/009/index.js
+++ desafios/009/index.js
@@ -7,7 +7,9 @@ function adicionar() {
     anot.innerHTML +=  `<ul class="del" id="${c}"> <li> <p >${texto}  <input id="botao"  type="button" value="X" onclick="deletar(${c})"></p> </li> </ul> `
     txt.value = ''
     txt.focus()
-   
+    if(texto==''){
+        anot.innerHTML = ''
+    }   
 }
 function deletar(id){
     let tarefa = document.getElementById(id)
